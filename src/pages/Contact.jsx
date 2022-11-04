bbf3e9b2560d738298dc8a73a81971d1140e0f58
Kofo <kajotonis@gmail.com>
2022-11-05 00:44:04 +0100

    feat: made adjustment to checkbox

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,8 +37,8 @@ const Contact = () => {
         </div>
 
      <div className = 'flex mt-[24px] '>
-    <input id='checkbox' name = 'checkbox' type = 'checkbox' className = ''/>
-    <p className = 'ml-3 mt-5 lg:mt-[0px]'>You agree to providing your data to {name} who may contact you. </p>
+    <input id='checkbox' name = 'checkbox' type = 'checkbox' className = 'relative mb-5 lg:mb-0'/>
+    <p className = 'ml-3  lg:mt-[0px]'>You agree to providing your data to {name} who may contact you. </p>
      </div>
         <button type = 'submit' id='btn__submit' href='/contact' className="text-center bg-blue-600 h-[48px] p-3 w-[343px] lg:w-[720px] rounded-[8px] mt-[32px]  text-white">Send message</button>
      </form>
